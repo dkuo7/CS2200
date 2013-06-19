cadb615b85bd3fe323620ae7fa3f9144ca710a98
Eric Peyton <ea.peyton@gmail.com>
2013-06-18 22:19:32 -0400

    Assembling.

--- Project3/assemble.py
+++ Project3/assemble.py
@@ -16,7 +16,8 @@ regs = {
 	'$ra':'f',
 	'$sp':'e',
 	'$at':'d',
-	'$zero':'0'
+	'$zero':'0',
+	'$v0':'a'
 }
 oppat = re.compile(r'\b(' + '|'.join(ops.keys()) + r')\b')
 start = False
@@ -26,7 +27,16 @@ for line in f.readlines():
 		for key in regs:
 			line = line.replace(key, regs[key])
 		line = line.replace(',','')
+		res = re.findall(r'\$a[0-9]',line)
+		for pat in res:
+			sub = str(int(pat[-1])+1)
+			line = re.sub('\\' + pat, sub, line)
+		line = re.sub(r'(\-?[0-9])\((.*)\)',r'\2 \1',line)
+		line = line.split('!')[0]
+		if(line[-1] != '\n'):
+			line = line + '\n'
 		if not re.match(r'^\s*$', line) and line[:3] != 'POW':
 			n.write(line[2:].lstrip())
+
 	if line[:4] == 'main':
 		start = True
